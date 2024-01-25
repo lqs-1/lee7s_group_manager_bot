79196f02c1fbdfe618b86bc6c3aa14b828a41514
lqs-1 <749062870@qq.com>
2024-01-25 15:45:24 +0800

    添加定时任务

--- app/handler/__init__.py
+++ app/handler/__init__.py
@@ -2,19 +2,22 @@
 # MessageHandler:消息处理器
 import logging
 
-from telegram.ext import MessageHandler, filters
+from telegram.ext import MessageHandler, filters, CommandHandler
 from telegram.ext._application import Application
-from app.handler.commandHandler import auto_delete_bot_message, new_chat_members
+from app.handler.commandHandler import auto_delete_bot_message, new_chat_members, set_timer, unset
 
 
 def register_all_handler(application: Application):
     # 添加命令处理器
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
 
     # logging.warning("application register 'new_member_join' handler")
     # application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_chat_members))
 
     # logging.warning("application register 'auto_delete_bot_message' handler")
     application.add_handler(MessageHandler(filters.TEXT, auto_delete_bot_message))
+
 
 
 
--- app/handler/commandHandler.py
+++ app/handler/commandHandler.py
@@ -1,7 +1,9 @@
 # Update:从Telegram获取更新
 import logging
 
+import requests
 from telegram import Update
+from telegram.constants import ParseMode
 # ContextTypes:上下文类型
 from telegram.ext import ContextTypes
 
@@ -31,3 +33,105 @@ async def auto_delete_bot_message(update: Update, context: ContextTypes.DEFAULT_
 
     except Exception as e:
         pass
+
+
+# 定时任务逻辑
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """定时任务回调函数"""
+    job = context.job
+
+    # 发起获取字典的请求
+    response = requests.get("https://nobibibi.top/back/sysDict/requestDictByParent/telegram_copy_dict")
+    response_data = response.json().get("parentDictAllSonDict")
+
+    # 获取有权执行命令的用户id 发送到这些id
+    bot_member_count_send_ids = response_data.get('bot_member_count_send_ids').split(":")
+    # 被统计的频道名
+    bot_member_count_names = response_data.get('bot_member_count_channel_names').split(":")
+
+    result = str()
+
+    for bot_member_count_name in bot_member_count_names:
+        chat_data = await context.bot.get_chat(chat_id=bot_member_count_name)
+        chat_count = await context.bot.get_chat_member_count(chat_id=bot_member_count_name)
+        result += f"[{chat_data.title}]({chat_data.link}): `{chat_count}`\n\n"
+
+
+    for send_id in bot_member_count_send_ids:
+        await context.bot.send_message(chat_id=send_id, text=result, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
+# 如果定时任务存在就删除
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """删除定时任务"""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# 设置定时任务时间
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """添加一个定时任务"""
+
+    # 发起获取字典的请求
+    response = requests.get("https://nobibibi.top/back/sysDict/requestDictByParent/telegram_copy_dict")
+    response_data = response.json().get("parentDictAllSonDict")
+
+    # 获取有权执行命令的用户id 这些id可以控制
+    bot_member_count_send_ids = response_data.get('bot_member_count_send_ids').split(":")
+    send_ids = list()
+    for send_id in bot_member_count_send_ids:
+        send_ids.append(int(send_id))
+
+    # 如果是开发者就返回机器人信息 如果是其他就返回错误信息
+    if update.message.chat.id in send_ids:
+        chat_id = update.effective_chat.id
+        try:
+            # 获取定时间隔时间
+            timer = float(context.args[0])
+            if timer < 0:
+                await update.effective_message.reply_text("定时时间不能小于1秒")
+                return
+
+            job_removed = remove_job_if_exists(str(chat_id), context)
+            context.job_queue.run_repeating(alarm, interval=timer, first=1, name=str(chat_id))
+
+            text = "定时任务设置成功"
+            if job_removed:
+                text += " 且删除了旧任务"
+            await update.effective_message.reply_text(text)
+
+        except (IndexError, ValueError):
+            await update.effective_message.reply_text("设置失败 请重新设置")
+
+    else:
+        logging.info(f"用户 {update.message.chat.username} 妄图设置定时任务")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="无权限")
+
+
+# 取消定时任务
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """取消定时任务."""
+
+    # 发起获取字典的请求
+    response = requests.get("https://nobibibi.top/back/sysDict/requestDictByParent/telegram_copy_dict")
+    response_data = response.json().get("parentDictAllSonDict")
+
+    # 获取有权执行命令的用户id 这些id可以控制
+    bot_member_count_send_ids = response_data.get('bot_member_count_send_ids').split(":")
+    send_ids = list()
+    for send_id in bot_member_count_send_ids:
+        send_ids.append(int(send_id))
+
+    if update.message.chat.id in send_ids:
+        chat_id = update.effective_chat.id
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        text = "任务取消成功" if job_removed else "你没有任务可以取消"
+        await update.message.reply_text(text)
+
+    else:
+        logging.info(f"用户 {update.message.chat.username} 妄图取消定时任务")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="无权限")
